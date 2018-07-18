07e1400f7d2e19232d94e650444c58f1c9e4048a
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-18 17:59:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
   if array.length == 1
-    return "#{array[0]}"
+    "#{array[0]}"
   elsif array.length == 2
-    return array.join(" and ")
+    array.join(" and ")
   elsif array.length >= 3
-    return array[-1] << "and "
-    array.join(", ")
+    return 
   end
 end
